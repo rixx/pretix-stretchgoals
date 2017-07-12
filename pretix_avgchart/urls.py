8c2e35001781b096e5430a135e9d6f955aaa82db
Tobias Kunze <rixx@cutebit.de>
2017-07-12 18:59:28 +0200

    Add skeleton urls and views

---
+++ pretix_avgchart/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from .views import ChartView, SettingsView
+
+urlpatterns = [
+    url(r'^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/avgchart/settings/', SettingsView.as_view(), name='chart'),
+    url(r'^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/avgchart/', ChartView.as_view(), name='settings'),
+]
---
+++ pretix_avgchart/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import View
+
+
+class ChartView(View):
+    pass
+
+
+class SettingsView(View):
+    pass
