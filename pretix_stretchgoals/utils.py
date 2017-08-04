64ab6dc7268224b6368de455999388a256a4ba58
Tobias Kunze <rixx@cutebit.de>
2017-08-04 23:53:08 +0200

    Add utils to get/set goals from settings

---
+++ pretix_stretchgoals/utils.py
@@ -0,0 +1,16 @@
+import json
+
+from i18nfield.strings import LazyI18nString
+from i18nfield.utils import I18nJSONEncoder
+
+
+def get_goals(event):
+    goals = json.loads(event.settings.get('stretchgoals_goals') or '[]')
+    for goal in goals:
+        goal['name'] = LazyI18nString(goal['name'])
+        goal['description'] = LazyI18nString(goal['description'])
+    return goals
+
+
+def set_goals(event, goals):
+    event.settings.set('stretchgoals_goals', json.dumps(goals, cls=I18nJSONEncoder))
