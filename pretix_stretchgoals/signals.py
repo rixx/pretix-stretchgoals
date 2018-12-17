668bb917a567b7a252f9566305d0a6a361c439ca
Raphael Michel <mail@raphaelmichel.de>
2018-12-17 22:46:01 +0100

    Add signal receiver for copying events

--- pretix_stretchgoals/signals.py
+++ pretix_stretchgoals/signals.py
@@ -1,9 +1,12 @@
+from django.db.models import QuerySet
 from django.urls import resolve, reverse
 from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 from i18nfield.strings import LazyI18nString
 
+from pretix.base.models import Item
 from pretix.base.settings import settings_hierarkey
+from pretix.base.signals import event_copy_data
 from pretix.control.signals import nav_event
 
 
@@ -19,6 +22,21 @@ def navbar_info(sender, request, **kwargs):
         }),
         'active': url.namespace == 'plugins:pretix_stretchgoals',
     }]
+
+
+@receiver(signal=event_copy_data, dispatch_uid="stretchgoals_copy_data")
+def event_copy_data_receiver(sender, other, item_map, **kwargs):
+    other.settings._h.add_type(
+        QuerySet,
+        lambda queryset: ','.join([str(element.pk) for element in queryset]),
+        lambda pk_list: [Item.objects.get(pk=element) for element in pk_list.split(',') if element]
+    )
+    initial_items = other.settings.get('stretchgoals_items', as_type=QuerySet) or []
+    if isinstance(initial_items, str) and initial_items:
+        initial_items = [int(i) for i in initial_items.split(',')]
+    else:
+        initial_items = [i.pk for i in initial_items]
+    sender.settings.stretchgoals_items = ','.join(str(item_map.get(i).pk) for i in initial_items if i in item_map)
 
 
 settings_hierarkey.add_default('stretchgoals_public_text', '', LazyI18nString)
